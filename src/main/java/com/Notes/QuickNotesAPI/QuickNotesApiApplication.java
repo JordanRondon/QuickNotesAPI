193fa8558cf4e5fc6d94740b82cbc74c49352f64
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-12-30 18:12:02 -0500

    first commit

---
+++ src/main/java/com/Notes/QuickNotesAPI/QuickNotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.Notes.QuickNotesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickNotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Notes/QuickNotesAPI/QuickNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Notes.QuickNotesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickNotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
