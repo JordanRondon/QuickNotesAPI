e16df40f86360d7754d01b9cf6c1976de93ebf99
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-01-02 12:31:50 -0500

    feat(controller: UsersController): Integration of controller methods for users.

---
+++ src/main/java/com/Notes/QuickNotesAPI/controller/UsersController.java
@@ -0,0 +1,10 @@
+package com.Notes.QuickNotesAPI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/User")
+public class UsersController {
+
+}
