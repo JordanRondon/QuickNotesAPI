19b018cbdb15f46f087ea7fa6b47334e32fa2997
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-01-06 11:15:19 -0500

    feat(NoteService, NoteController): Integration of methods to insert a note per user

--- src/main/java/com/QuickNotesAPI/QuickNotes/controller/NoteController.java
+++ src/main/java/com/QuickNotesAPI/QuickNotes/controller/NoteController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,18 @@ public class NoteController {
             return ResponseEntity.ok(listNote);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PostMapping("/save/{userId}")
+    private ResponseEntity<String> save(@RequestBody Note note, @PathVariable int userId) {
+        try {
+            noteService.save(note, userId);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Note registered successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An unexpected error occurred: " + e.getMessage());
         }
     }
 
--- src/main/java/com/QuickNotesAPI/QuickNotes/service/NoteService.java
+++ src/main/java/com/QuickNotesAPI/QuickNotes/service/NoteService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.QuickNotesAPI.QuickNotes.model.Note;
+import com.QuickNotesAPI.QuickNotes.model.Users;
 import com.QuickNotesAPI.QuickNotes.repository.INoteRepository;
 import com.QuickNotesAPI.QuickNotes.service.Interface.INoteService;
+import com.QuickNotesAPI.QuickNotes.service.Interface.IUsersService;
 
 @Service
 public class NoteService implements INoteService {
@@ -15,9 +17,19 @@ public class NoteService implements INoteService {
     @Autowired
     private INoteRepository noteRepository;
 
+    @Autowired
+    private IUsersService usersService;
+
     @Override
     public List<Note> getListNoteByUserId(int userId) {
         return noteRepository.getListNoteByUserId(userId);
+    }
+
+    @Override
+    public void save(Note note, int userId) {
+        Users user = usersService.getUserById(userId);
+        note.setUser(user);
+        noteRepository.save(note);
     }
 
 }
