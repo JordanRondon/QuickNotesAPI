3bbbe23c80e0f597e9747c881d2b91441731ad70
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-01-07 12:18:35 -0500

    feat(pom, properties, JwtUtil): Integration of dependencies and methods to generate a JWT

---
+++ src/main/java/com/QuickNotesAPI/QuickNotes/util/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.QuickNotesAPI.QuickNotes.util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.QuickNotesAPI.QuickNotes.model.Users;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    private Key secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateJWT(Users user) {
+        Map<String, Object> headers = new HashMap<>();
+
+        headers.put("alg", "HS256");
+        headers.put("typ", "JWT");
+
+        return "Bearer " + Jwts.builder()
+                .setHeader(headers)
+                .setSubject(user.getEmail())
+                .claim("id", user.getUsersId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 6)) // 6 hours
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
