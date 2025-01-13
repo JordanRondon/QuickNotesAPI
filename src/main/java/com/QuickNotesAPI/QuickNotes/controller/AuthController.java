f6c3d1ff17436cc684efacdff69804045eb06dab
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-01-13 11:34:08 -0500

    docs(AuthController): AuthController class methods documentation

--- src/main/java/com/QuickNotesAPI/QuickNotes/controller/AuthController.java
+++ src/main/java/com/QuickNotesAPI/QuickNotes/controller/AuthController.java
@@ -11,12 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.QuickNotesAPI.QuickNotes.model.Users;
 import com.QuickNotesAPI.QuickNotes.service.UsersService;
 
+/**
+ * REST controller for handling authentication-related operations, such as user
+ * registration and login.
+ */
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     private UsersService usersService;
 
+    /**
+     * Registers a new user in the system.
+     *
+     * @param user the user to register, provided in the request body
+     * @return a response entity with a success message and HTTP status 201 if the
+     *         user is registered successfully,
+     *         or an error message and HTTP status 500 if an exception occurs
+     */
     @PostMapping("/register")
     private ResponseEntity<String> register(@RequestBody Users user) {
         try {
@@ -29,6 +41,16 @@ public class AuthController {
         }
     }
 
+    /**
+     * Authenticates a user by validating their email and password, and returns a
+     * JWT upon successful login.
+     *
+     * @param user the user credentials, including email and password, provided in
+     *             the request body
+     * @return a response entity with the JWT token and HTTP status 200 if login is
+     *         successful,
+     *         or an error message and HTTP status 500 if an exception occurs
+     */
     @PostMapping("/login")
     private ResponseEntity<String> login(@RequestBody Users user) {
         try {
