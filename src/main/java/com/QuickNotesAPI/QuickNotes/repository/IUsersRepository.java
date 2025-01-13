21a1c46ffa9abd8801c797af146af6df6cab6453
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-01-13 11:07:28 -0500

    docs(IUsersRepository): IUsersRepository repository class documentation

--- src/main/java/com/QuickNotesAPI/QuickNotes/repository/IUsersRepository.java
+++ src/main/java/com/QuickNotesAPI/QuickNotes/repository/IUsersRepository.java
@@ -11,14 +11,37 @@ import com.QuickNotesAPI.QuickNotes.model.Users;
 
 import jakarta.transaction.Transactional;
 
+/**
+ * Repository interface for performing CRUD operations and custom queries on the
+ * Users entity.
+ */
 public interface IUsersRepository extends JpaRepository<Users, Integer> {
 
+    /**
+     * Retrieves a list of users whose last names start with the specified initial.
+     *
+     * @param lastNameInitial the initial of the last name to search for
+     * @return a list of users matching the criteria
+     */
     @Query("SELECT us FROM Users us WHERE us.lastName LIKE CONCAT(:lastNameInitial, '%')")
     public List<Users> getListUsersByLastNameInit(@Param("lastNameInitial") String lastNameInitial);
 
+    /**
+     * Finds a user by their email if the user is marked as visible.
+     *
+     * @param email the email of the user to search for
+     * @return the user matching the email, or null if not found or not visible
+     */
     @Query("SELECT us FROM Users us WHERE us.email = :email AND us.visible = true")
     public Users findByEmail(@Param("email") String email);
 
+    /**
+     * Disables a user by setting their visible attribute to false.
+     *
+     * This operation is transactional and modifies the database.
+     *
+     * @param userId the ID of the user to disable
+     */
     @Modifying
     @Transactional
     @Query("UPDATE Users us SET us.visible = false WHERE us.usersId = :userId")
