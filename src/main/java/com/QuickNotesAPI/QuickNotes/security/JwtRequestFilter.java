0537288b2361cc3ddaeb04fa879cf23ad86b3dc6
JordanRondon <jordanangelrondonpozo@gmail.com>
2025-01-13 12:11:58 -0500

    docs(JwtRequestFilter): JwtRequestFilter class methods documentation

--- src/main/java/com/QuickNotesAPI/QuickNotes/security/JwtRequestFilter.java
+++ src/main/java/com/QuickNotesAPI/QuickNotes/security/JwtRequestFilter.java
@@ -21,12 +21,35 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * Security filter that intercepts HTTP requests to verify the validity of the
+ * JWT
+ * in the Authorization header and authenticate the user based on the JWT token.
+ *
+ * Extends "OncePerRequestFilter" to ensure that the filter is executed only
+ * once
+ * per request.
+ */
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtil jwtUtil;
 
+    /**
+     * Method that intercepts HTTP requests and validates the JWT token in the
+     * Authorization header.
+     * If the token is valid, sets up authentication in the Spring security context.
+     *
+     * @param request:     The HTTP request containing the JWT token in the
+     *                     Authorization header.
+     * @param response:    The HTTP response used to send error codes if the token
+     *                     is invalid.
+     * @param filterChain: The filter chain that allows the request to continue
+     *                     processing.
+     * @throws ServletException: If an error occurs in filter processing.
+     * @throws IOException:      If an error occurs in data input or output.
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -39,11 +62,17 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 String email = jwtUtil.extractEmail(token);
                 String role = jwtUtil.extractRole(token);
 
+                // If the email is valid and the user is not authenticated, the user is
+                // authenticated
                 if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    // Create a list of authorities (roles) for the user
                     List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_" + role);
+                    // Create an authentication object with the user's email and roles
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                             email, null, authorities);
+                    // Sets the authentication details in the request
                     authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    // Sets the authentication context to the Spring security context
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
@@ -65,6 +94,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
+    /**
+     * Method that gets the JWT token from the Authorization header of the request.
+     *
+     * @param request: The HTTP request containing the Authorization header.
+     * @return JWT if present in the header, or `null` if not found.
+     */
     private String getTokenFromHeader(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
@@ -73,6 +108,14 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         return null;
     }
 
+    /**
+     * Method that validates whether the JWT token has expired.
+     *
+     * @param token:    The JWT token to validate.
+     * @param response: The HTTP response used to return an error if the token has
+     *                  expired.
+     * @throws IOException: If an error occurs while writing the error response.
+     */
     private void validateToken(String token, HttpServletResponse response) throws IOException {
         if (jwtUtil.isTokenExpired(token)) {
             throw new ExpiredJwtException(null, null, "Token has expired");
